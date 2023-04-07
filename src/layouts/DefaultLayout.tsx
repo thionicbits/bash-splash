73eb117f0891dfbd6ec887af56478fddb9b75936
thionicbits <thionicbits@proton.me>
2023-04-07 12:32:48 +0200

    Creating the Page Navigation block

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,7 +1,13 @@
+import PageNavigation from '../components/blocks/PageNavigation';
 interface IProps {
   children: React.ReactNode;
 }
 
-const DefaultLayout = ({ children }: IProps): JSX.Element => <>{children}</>;
+const DefaultLayout = ({ children }: IProps): JSX.Element => (
+  <>
+    <PageNavigation />
+    {children}
+  </>
+);
 
 export default DefaultLayout;
